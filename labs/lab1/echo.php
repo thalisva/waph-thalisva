4cf1dab0989659b3e821d30626ce73e6bca21cb4
thalisva <156384518+thalisva@users.noreply.github.com>
2024-02-08 10:25:17 -0500

    revised echo.php

--- labs/lab1/echo.php
+++ labs/lab1/echo.php
@@ -1,5 +1,5 @@
 <?php
-      echo $_REQUEST["data"];
+      
       if(empty($_REQUEST["data"])){
          exit("please enter the input field 'data'");
          $input=htmlentities($_REQUEST["data"]);
